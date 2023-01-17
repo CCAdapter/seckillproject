8b321b1e9faf25908c889ddc5ac9fe3c25f65db0
LeonLyu <45918751+LeonLyu@users.noreply.github.com>
2023-01-18 01:52:10 +0800

    删除MyBatis-generator工具自动生成的DOExample类

--- src/main/java/com/seckillproject/App.java
+++ src/main/java/com/seckillproject/App.java
@@ -5,7 +5,6 @@ import com.seckillproject.dataobject.UserDO;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +30,6 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         SpringApplication.run(App.class, args);
     }
 }
