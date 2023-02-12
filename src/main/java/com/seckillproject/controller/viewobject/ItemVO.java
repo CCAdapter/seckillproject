2b86ad42c47ec6603d50ea6ad541034ab5bd1b4c
LeonLyu <45918751+LeonLyu@users.noreply.github.com>
2023-02-12 23:50:11 +0800

    向前端返回商品ID字段

--- src/main/java/com/seckillproject/controller/viewobject/ItemVO.java
+++ src/main/java/com/seckillproject/controller/viewobject/ItemVO.java
@@ -1,11 +1,10 @@
 package com.seckillproject.controller.viewobject;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 public class ItemVO {
+    private Integer id;
+
     // 商品名称
     private String title;
 
@@ -70,5 +69,13 @@ public class ItemVO {
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 }
