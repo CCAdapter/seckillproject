b9c0464af7781a09865b6b49ce4a8676604a46ea
LeonLyu <45918751+LeonLyu@users.noreply.github.com>
2023-02-20 01:21:50 +0800

    数据库备份SQL文件

--- seckill.sql
+++ seckill.sql
@@ -1,26 +1,35 @@
-/*
- Navicat MySQL Data Transfer
+-- MySQL dump 10.13  Distrib 5.7.29, for macos10.14 (x86_64)
+--
+-- Host: localhost    Database: seckill
+-- ------------------------------------------------------
+-- Server version	5.7.29
 
- Source Server         : Mysql
- Source Server Type    : MySQL
- Source Server Version : 50729
- Source Host           : localhost:3306
- Source Schema         : seckill
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
- Target Server Type    : MySQL
- Target Server Version : 50729
- File Encoding         : 65001
+--
+-- Current Database: `seckill`
+--
 
- Date: 19/02/2023 01:37:09
-*/
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `seckill` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+USE `seckill`;
 
--- ----------------------------
--- Table structure for item
--- ----------------------------
+--
+-- Table structure for table `item`
+--
+
 DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `item` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -30,40 +39,50 @@ CREATE TABLE `item` (
   `img_url` varchar(500) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of item
--- ----------------------------
-BEGIN;
-INSERT INTO `item` VALUES (1, 'iphone', 12.00, 'iphone14', 7, 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660753617559');
-INSERT INTO `item` VALUES (7, '三星S23', 12.87, '三星S23', 1, 'https://shop-image.samsung.com.cn/goods/2023/02/01/63d9434ce4b08db2edda2201.png?x-oss-process=image/resize,w_450,m_lfit');
-INSERT INTO `item` VALUES (8, 'huawei mate', 10888.00, 'huawei', 1, 'https://p0.itc.cn/q_70/images03/20220828/d42b1775eac54c6d8d8df449de67b1d4.png');
-COMMIT;
+--
+-- Dumping data for table `item`
+--
 
--- ----------------------------
--- Table structure for item_stock
--- ----------------------------
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,'iphone',12.00,'iphone14',7,'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-14-pro-model-unselect-gallery-2-202209?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=1660753617559'),(7,'三星S23',12.87,'三星S23',1,'https://shop-image.samsung.com.cn/goods/2023/02/01/63d9434ce4b08db2edda2201.png?x-oss-process=image/resize,w_450,m_lfit'),(8,'huawei mate',10888.00,'huawei',1,'https://p0.itc.cn/q_70/images03/20220828/d42b1775eac54c6d8d8df449de67b1d4.png');
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item_stock`
+--
+
 DROP TABLE IF EXISTS `item_stock`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `item_stock` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `stock` int(11) NOT NULL DEFAULT '0',
   `item_id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of item_stock
--- ----------------------------
-BEGIN;
-INSERT INTO `item_stock` VALUES (1, 115, 1);
-INSERT INTO `item_stock` VALUES (7, 72, 7);
-INSERT INTO `item_stock` VALUES (8, 99, 8);
-COMMIT;
+--
+-- Dumping data for table `item_stock`
+--
 
--- ----------------------------
--- Table structure for order_info
--- ----------------------------
+LOCK TABLES `item_stock` WRITE;
+/*!40000 ALTER TABLE `item_stock` DISABLE KEYS */;
+INSERT INTO `item_stock` VALUES (1,115,1),(7,72,7),(8,99,8);
+/*!40000 ALTER TABLE `item_stock` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_info`
+--
+
 DROP TABLE IF EXISTS `order_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `order_info` (
   `id` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `user_id` int(11) NOT NULL DEFAULT '0',
@@ -74,26 +93,25 @@ CREATE TABLE `order_info` (
   `promo_id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of order_info
--- ----------------------------
-BEGIN;
-INSERT INTO `order_info` VALUES ('2023021600000300', 18, 1, 12, 1, 12, 0);
-INSERT INTO `order_info` VALUES ('2023021600000400', 18, 1, 12, 1, 12, 0);
-INSERT INTO `order_info` VALUES ('2023021600000500', 18, 1, 12, 1, 12, 0);
-INSERT INTO `order_info` VALUES ('2023021600000600', 18, 1, 12, 1, 12, 0);
-INSERT INTO `order_info` VALUES ('2023021600000700', 18, 7, 12.87, 1, 12.87, 0);
-INSERT INTO `order_info` VALUES ('2023021600000800', 18, 8, 10888, 1, 10888, 0);
-INSERT INTO `order_info` VALUES ('2023021800001200', 18, 1, 5, 1, 5, 1);
-INSERT INTO `order_info` VALUES ('2023021800001300', 18, 1, 5, 1, 5, 1);
-INSERT INTO `order_info` VALUES ('2023021800001400', 18, 1, 5, 1, 5, 1);
-COMMIT;
+--
+-- Dumping data for table `order_info`
+--
 
--- ----------------------------
--- Table structure for promo
--- ----------------------------
+LOCK TABLES `order_info` WRITE;
+/*!40000 ALTER TABLE `order_info` DISABLE KEYS */;
+INSERT INTO `order_info` VALUES ('2023021600000300',18,1,12,1,12,0),('2023021600000400',18,1,12,1,12,0),('2023021600000500',18,1,12,1,12,0),('2023021600000600',18,1,12,1,12,0),('2023021600000700',18,7,12.87,1,12.87,0),('2023021600000800',18,8,10888,1,10888,0),('2023021800001200',18,1,5,1,5,1),('2023021800001300',18,1,5,1,5,1),('2023021800001400',18,1,5,1,5,1);
+/*!40000 ALTER TABLE `order_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `promo`
+--
+
 DROP TABLE IF EXISTS `promo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `promo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `promo_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -103,36 +121,50 @@ CREATE TABLE `promo` (
   `end_date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of promo
--- ----------------------------
-BEGIN;
-INSERT INTO `promo` VALUES (1, 'iphone抢购活动', '2023-02-18 23:55:50', 1, 5, '2023-02-19 23:00:00');
-COMMIT;
+--
+-- Dumping data for table `promo`
+--
 
--- ----------------------------
--- Table structure for sequence_info
--- ----------------------------
+LOCK TABLES `promo` WRITE;
+/*!40000 ALTER TABLE `promo` DISABLE KEYS */;
+INSERT INTO `promo` VALUES (1,'iphone抢购活动','2023-02-18 23:55:50',1,5,'2023-02-19 23:00:00');
+/*!40000 ALTER TABLE `promo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sequence_info`
+--
+
 DROP TABLE IF EXISTS `sequence_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sequence_info` (
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `current_value` int(11) NOT NULL DEFAULT '0',
   `step` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of sequence_info
--- ----------------------------
-BEGIN;
-INSERT INTO `sequence_info` VALUES ('order_info', 15, 1);
-COMMIT;
+--
+-- Dumping data for table `sequence_info`
+--
 
--- ----------------------------
--- Table structure for user_info
--- ----------------------------
+LOCK TABLES `sequence_info` WRITE;
+/*!40000 ALTER TABLE `sequence_info` DISABLE KEYS */;
+INSERT INTO `sequence_info` VALUES ('order_info',15,1);
+/*!40000 ALTER TABLE `sequence_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_info`
+--
+
 DROP TABLE IF EXISTS `user_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
@@ -144,30 +176,50 @@ CREATE TABLE `user_info` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `telphone_unique_index` (`telphone`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of user_info
--- ----------------------------
-BEGIN;
-INSERT INTO `user_info` VALUES (18, 'abc', 1, 12, '123456', 'byphone', '');
-COMMIT;
+--
+-- Dumping data for table `user_info`
+--
 
--- ----------------------------
--- Table structure for user_password
--- ----------------------------
+LOCK TABLES `user_info` WRITE;
+/*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
+INSERT INTO `user_info` VALUES (18,'abc',1,12,'123456','byphone','');
+/*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_password`
+--
+
 DROP TABLE IF EXISTS `user_password`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_password` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `encrpt_password` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `user_id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of user_password
--- ----------------------------
-BEGIN;
-INSERT INTO `user_password` VALUES (6, 'ICy5YqxZB1uWSwcVLSNLcA==', 18);
-COMMIT;
+--
+-- Dumping data for table `user_password`
+--
 
-SET FOREIGN_KEY_CHECKS = 1;
+LOCK TABLES `user_password` WRITE;
+/*!40000 ALTER TABLE `user_password` DISABLE KEYS */;
+INSERT INTO `user_password` VALUES (6,'ICy5YqxZB1uWSwcVLSNLcA==',18);
+/*!40000 ALTER TABLE `user_password` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-02-20  1:19:08
