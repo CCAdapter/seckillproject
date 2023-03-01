fcc39ca8350f2d3368631d4a17d822287c96c524
LeonLyu <45918751+LeonLyu@users.noreply.github.com>
2023-03-02 01:31:26 +0800

    商品详情查询接口接入Redis缓存，自定义Redis存取的序列化和反序列化方式

--- src/main/java/com/seckillproject/config/RedisConfig.java
+++ src/main/java/com/seckillproject/config/RedisConfig.java
@@ -1,9 +1,47 @@
 package com.seckillproject.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.seckillproject.serializer.JodaDateTimeJsonDeserializer;
+import com.seckillproject.serializer.JodaDateTimeJsonSerializer;
+import org.joda.time.DateTime;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
 public class RedisConfig {
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 解决key的序列化方式
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+
+        // 解决value的序列化方式
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        // 设置定制化的JodaDateTime序列化和反序列化方法，并注册到jackson2JsonRedisSerializer容器
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(DateTime.class, new JodaDateTimeJsonSerializer());
+        simpleModule.addDeserializer(DateTime.class, new JodaDateTimeJsonDeserializer());
+
+        // 在序列化结果中加入类的信息，否则会反序列化失败
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+        objectMapper.registerModule(simpleModule);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+
+        return redisTemplate;
+    }
 }
--- src/main/java/com/seckillproject/controller/ItemController.java
+++ src/main/java/com/seckillproject/controller/ItemController.java
@@ -8,11 +8,13 @@ import com.seckillproject.service.model.ItemModel;
 import org.joda.time.format.DateTimeFormat;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Controller("item")
@@ -22,6 +24,9 @@ public class ItemController extends BaseController {
 
     @Autowired
     private ItemService itemService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     // 创建商品
     @RequestMapping(value = "/create", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
@@ -48,7 +53,18 @@ public class ItemController extends BaseController {
     @RequestMapping(value = "/get", method = {RequestMethod.GET})
     @ResponseBody
     public CommonReturnType getItem(@RequestParam(name = "id") Integer id) {
-        ItemModel itemModel = itemService.getItemById(id);
+
+        // 根据商品的id到redis内获取
+        Object o = redisTemplate.opsForValue().get("item_" + id);
+        ItemModel itemModel = (ItemModel) o;
+
+        // 若redis内不存在对应的itemModel，则访问下游service
+        if (itemModel == null) {
+            itemModel = itemService.getItemById(id);
+            redisTemplate.opsForValue().set("item_" + id, itemModel);
+            redisTemplate.expire("item_" + id, 10, TimeUnit.MINUTES);
+        }
+
         ItemVO itemVO = convertVOFromModel(itemModel);
         return CommonReturnType.create(itemVO);
     }
